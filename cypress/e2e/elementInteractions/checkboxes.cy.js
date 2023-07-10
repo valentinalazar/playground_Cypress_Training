e360bc4d39f8acc6ff51af9200005a4fb5434093
Valentina-Lazar_tvlport <Valentina.Lazar@travelport.com>
2023-07-10 13:21:13 +0300

    Web Automation Testing with Cypress - Advanced
    Web Automation Testing with Cypress - Advanced

--- cypress/e2e/elementInteractions/checkboxes.cy.js
+++ cypress/e2e/elementInteractions/checkboxes.cy.js
@@ -1,17 +1,21 @@
+
+import '../../fixtures/commands.js'
 describe('Checkboxes', () => {
 
     beforeEach('before each', ()=>{
         cy.visit('http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html');
         cy.get('[id="dropdowm-menu-1"]').should('be.visible');
     })
+
     it('Checks/Unchecks 1 checkbox', () => {
-        cy.get('[value="option-1"]')
+        cy.getInputByValue('option-1')
             .should('be.visible')
             .check()
             .and('be.checked')
             .uncheck()
             .and('not.be.checked');
     });
+
     it('Checks/UnchecksOption 2 checkbox', () => {
         cy.get('input[type="checkbox"]').first().should('be.visible').check().and('be.checked').uncheck().and('not.be.checked');
     });
